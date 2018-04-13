78a835f63176caccb22498a459958880f30c7ad8
liumapp <liumapp.com@gmail.com>
2018-04-13 16:27:57 +0800

    rm unnecessary things

---
+++ src/main/java/com/liumapp/demo/proxy/Proxy.java
@@ -0,0 +1,20 @@
+package com.liumapp.demo.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liumapp
+ * @file Proxy.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 4/13/18
+ */
+@SpringBootApplication
+public class Proxy {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Proxy.class , args);
+    }
+
+}
